7d095b245606ccf579e6636fd2508e64372889d7
culplate <culplate@gmail.com>
2024-07-05 14:00:48 +0200

    feat: added navigation

--- src/modules/AppBar/Navigation/Navigation.jsx
+++ src/modules/AppBar/Navigation/Navigation.jsx
@@ -1,12 +1,36 @@
 import { NavLink } from "react-router-dom";
-// import css from "./Navigation.scss";
+import css from "./Navigation.module.scss";
+import clsx from "clsx";
 
 export const Navigation = () => {
   return (
     <nav>
-      <NavLink to={"/"}>Home</NavLink>
-      <NavLink to={"/catalogue"}>Catalogue</NavLink>
-      <NavLink to={"/favorites"}>Favorites</NavLink>
+      <NavLink
+        to={"/"}
+        className={({ isActive }) =>
+          clsx(css.link, isActive ? css.active : null)
+        }
+      >
+        Home
+      </NavLink>
+
+      <NavLink
+        to={"/catalogue"}
+        className={({ isActive }) =>
+          clsx(css.link, isActive ? css.active : null)
+        }
+      >
+        Catalogue
+      </NavLink>
+
+      <NavLink
+        to={"/favorites"}
+        className={({ isActive }) =>
+          clsx(css.link, isActive ? css.active : null)
+        }
+      >
+        Favorites
+      </NavLink>
     </nav>
   );
 };
