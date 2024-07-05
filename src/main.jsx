5648dc5f0f23906b87f14eee8e714f0a73bff58b
culplate <culplate@gmail.com>
2024-07-05 15:08:00 +0200

    feat: added persisted store

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
 import "modern-normalize";
+
+import { persistor, store } from "@redux/store.js";
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { ModalProvider } from "./context/ModalProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ModalProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ModalProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ModalProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ModalProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
